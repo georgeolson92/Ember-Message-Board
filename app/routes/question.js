926c6ba164f9f5e2dd7daa8af616392d719cdfdd
George <georgeolson92@gmail.com>
2016-08-19 08:26:51 -0700

    Add ability to delete answers

--- app/routes/question.js
+++ app/routes/question.js
@@ -22,6 +22,10 @@ export default Ember.Route.extend({
         return question.destroyRecord();
       });
       this.transitionTo('index');
+    },
+    destroyAnswer(answer) {
+      answer.destroyRecord();
+      this.transitionTo('question');
     }
   }
 });
